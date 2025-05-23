263d6af16ecd8cd788e80b03cf883c766d60d4ff
dainlucas <lucasdainn@gmail.com>
2025-05-23 04:34:40 +0000

    atualização site

--- js/menu.js
+++ js/menu.js
@@ -1,12 +1,16 @@
-document.querySelector(".menu-toggle").addEventListener("click", function () {
-  document.querySelector(".main-nav").classList.toggle("active");
+const menuToggle = document.querySelector(".menu-toggle");
+const mainNav = document.querySelector(".main-nav");
+
+menuToggle.addEventListener("click", function () {
+  console.log("Menu toggle clicked");
+  mainNav.classList.toggle("active");
   this.classList.toggle("active");
 });
 
 // Fecha o menu ao clicar em um link
 document.querySelectorAll(".main-nav a").forEach((link) => {
   link.addEventListener("click", () => {
-    document.querySelector(".main-nav").classList.remove("active");
-    document.querySelector(".menu-toggle").classList.remove("active");
+    mainNav.classList.remove("active");
+    menuToggle.classList.remove("active");
   });
 });
